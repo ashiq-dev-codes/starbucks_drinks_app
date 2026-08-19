9375b8421459825e71c8479f6078c9587416104c
ashiq <ashiq@mvp-apps.ae>
2026-08-19 23:03:57 +0400

    Remove RN-starbucks-drinks package.json, reset-project script, and tsconfig.json files to clean up the project structure.

--- react-native/RN-starbucks-drinks/constants/index.ts
+++
@@ -1,8 +0,0 @@
-import { Dimensions } from "react-native";
-
-export const { width, height } = Dimensions.get("window");
-
-export const ITEM_HEIGHT = 350;
-export const ITEM_WIDTH = width * 0.5;
-export const ITEM_SPACE = width - ITEM_WIDTH / 2;
-export const INFO_ITEM_HIEGHT = 385;
